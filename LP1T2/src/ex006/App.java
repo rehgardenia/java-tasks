44131bfa614a7c9af7500be2d07a3208b037899c
Gixya <giovannafgoncalves10@gmail.com>
2026-04-14 19:39:46 -0300

    Add functionality to input and display names
    Implement main method to input and display names in a 2x3 matrix.

--- LP1T2/src/ex006/App.java
+++ LP1T2/src/ex006/App.java
@@ -1,5 +1,32 @@
 package ex006;
 
-public class App {
-    
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // cria uma matriz 2x3 para armazenar nomes
+        String nomes[][] = new String[2][3];
+
+        // entrada de dados
+        for (int i = 0; i < 2; i++) { // percorre as linhas
+            for (int j = 0; j < 3; j++) { // percorre as colunas
+                System.out.print("Digite um nome para [" + i + "][" + j + "]: ");
+                nomes[i][j] = sc.nextLine();
+            }
+        }
+
+        // exibição dos nomes
+        System.out.println("\nNomes digitados:");
+
+        for (int i = 0; i < 2; i++) { // percorre as linhas
+            for (int j = 0; j < 3; j++) { // percorre as colunas
+                System.out.print(nomes[i][j] + " ");
+            }
+            System.out.println(); // quebra de linha ao final de cada linha
+        }
+
+        sc.close();
+    }
 }
