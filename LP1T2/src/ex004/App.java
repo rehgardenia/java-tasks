c34938de301829ad4b590486aec2b0f61b2020ac
Gixya <giovannafgoncalves10@gmail.com>
2026-04-14 19:05:25 -0300

    ex4
    Added a main class that initializes a 2x3 matrix, takes user input to populate it, and displays the matrix values.

--- LP1T2/src/ex004/App.java
+++ LP1T2/src/ex004/App.java
@@ -1,5 +1,32 @@
 package ex004;
 
-public class App {
-    
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // cria uma matriz 2x3 
+        int matriz[][] = new int[2][3];
+
+        // entrada de dados
+        for (int i = 0; i < 2; i++) { // percorre as linhas
+            for (int j = 0; j < 3; j++) { // percorre as colunas
+                System.out.print("Digite um valor para [" + i + "][" + j + "]: ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        // exibição da matriz
+        System.out.println("\nValores da matriz:");
+
+        for (int i = 0; i < 2; i++) { // percorre as linhas
+            for (int j = 0; j < 3; j++) { // percorre as colunas
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(); // pula linha após cada linha da matriz
+        }
+
+        sc.close();
+    }
 }
